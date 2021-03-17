1ff3bbebf8d09f0f23e4ca3635ddbcd418394fe2
jojo <jodiladouceur.design@gmail.com>
2021-03-17 14:55:22 -0400

    Edits to Index.html script.html and style.css

--- script.js
+++ script.js
@@ -1,2 +1,13 @@
+var timeSlots = document.querySelector(".container");
+var buttonClick = document.querySelector(".saveBtn");
+var buttonClick = document.querySelector(".textArea9");
+
+timeSlots.addEventListener("click", function(){
+    console.log("A button was clicked");
+});
+
+
+
+
 var today = moment();
 $("#currentDay").text(today.format("ddd MMM Do, YYYY"));
