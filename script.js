b1932fd14646314123c7dd050ca3a838d1517ab4
jojo <jodiladouceur.design@gmail.com>
2021-03-20 15:17:31 -0400

    Edits to index.html and script.js

--- script.js
+++ script.js
@@ -1,51 +1,44 @@
 //setting variables 
-var hoursList = {
-    hours: ["hour1", "hour2", "hour3", "hour4", "hour5", "hour6", "hour7", "hour8" ], 
-}
-
 var timeSlots = document.querySelector(".container");
 var buttonClick = document.querySelectorAll(".saveBtn");
-var inputArea9 = document.querySelector(".textArea9");
-var inputArea10 = document.querySelector(".textArea10");
 var input = document.querySelectorAll("textarea");
 
 var element;
+var currentHour = moment().format("hh mm");
+var pastHour;
+var thisHour = moment().format("hh mm");
+var nextHour;
+
 
 //log the array in the hoursList object
-console.log(hoursList.hours)
-
+console.log(input)
 // adding the date to the file
 var today = moment();
 $("#currentDay").text(today.format("ddd MMM Do, YYYY"));
 
+// loop the hours to select items from local storge
+input.forEach(function (hour) {
+    hour.value = localStorage.getItem(hour.id)
+    })
+
+//can get rid of console logs
 // adds an event listener to the save buttons
 timeSlots.addEventListener("click", function(event){
     element = event.target; 
     if (element.matches("button")){
         console.log("A button was clicked");
-        collectValue();
+        var id = element.dataset.textareaid
+        var textArea = document.getElementById(id)
+        localStorage.setItem(id, textArea.value)
     }; 
 });
 
-// function to catch the value entered into the text area
-function collectValue() {
-    console.log(input.value); 
-   // if (inputArea9 = input.value) {
-        hoursList.hours[0] = input.value;
-        console.log(hoursList.hours)
-        localStorage.setItem("hours", JSON.stringify(hoursList.hours))
-   // } else if (inputArea10 = input.value) {
-       // hoursList.hours[1] = input.value;
-       // console.log(hoursList.hours)
-       // localStorage.setItem("hours", JSON.stringify(hoursList.hours))
+console.log(thisHour)
+
+//function colorChange() {
+   // if (thisHour === currentHour){
+       // input.setAttribute("background: yellow")
    // }
+//}
 
-};
-
-//Put Stored items into the input boxes it doesn't work right now. 
-function replaceValue() {
-    localStorage.getItem("hours")
-    inputArea9.write = JSON.parse(hoursList.hours)
-}
-
-
+//colorChange ();
